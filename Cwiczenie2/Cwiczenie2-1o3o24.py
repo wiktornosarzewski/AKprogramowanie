4b94a1f5983987ad6a505cc84f4feec249d926a6
Wiktor Nosarzewski <wiktorn2003@gmail.com>
2024-03-01 10:59:32 +0100

    Add files via upload

--- Cwiczenie2/Cwiczenie2-1o3o24.py
+++ Cwiczenie2/Cwiczenie2-1o3o24.py
@@ -1,26 +1,30 @@
 a = float(input("podaj a = "))
 b = float(input("podaj b = "))
 c = float(input("podaj c = "))
-#By Wiktor Nosarzewski 1o3o24
+#Cw2 By Wiktor Nosarzewski 1o3o24 v2
 print(f"Dane jest równanie {a} x^2 + {b}x + {c} - 0")
 #np.4,5,1
-delta = b*b - 4*a*c
-
-if delta < 0:
-    print("Równanie nie posiada rozwiązań")
-    print(f"Delta = {delta}")
+#delta = b*b - 4*a*c
+if a == 0:
+    print(f"a = {a}")
+    #Dalszy ciąg sprawdzania
 else:
-    print("Obliczam pierwiastki")
-    if delta == 0:
-        x = -b / (2*a)
-        #print(f"x = {x}")
-        print(f"x = {x}")
+    delta = -b**2 - 4*a*c
+    if delta < 0:
+        print("Równanie nie posiada rozwiązań")
+        print(f"Delta = {delta}")
     else:
-        #x1 = (-b - math.sqrt(delta) / (2*a))
-        x1 = (-b - delta ** 0.5) / (2*a)
-        x2 = (-b + delta * 0.5) / (2*a)
-        print(f"x1 = {x1}")
-        print(f"x2 = {x2}")
+        print("Obliczam pierwiastki")
+        if delta == 0:
+            x = -b / (2*a)
+            #print(f"x = {x}")
+            print(f"x = {x}")
+        else:
+            #x1 = (-b - math.sqrt(delta) / (2*a))
+            x1 = (-b - delta ** 0.5) / (2*a)
+            x2 = (-b + delta * 0.5) / (2*a)
+            print(f"x1 = {x1}")
+            print(f"x2 = {x2}")
 
 print("Dziękujemy za użycie programu.")
-input()                        
+input()
